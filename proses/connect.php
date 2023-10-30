50a256cbdadf86353f354b5f728ed27a3e6bd4d0
Wulandari883 <112493973+Wulandari883@users.noreply.github.com>
2023-10-30 08:17:56 +0700

    Update connect.php

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost","u837952942_dyah","1Tx+UDM|","u837952942_dyah");
+    $conn = mysqli_connect("localhost","u837952942_dyah","OHji2~y5","u837952942_dyah");
     //if(!$conn){
     //    echo "Gagal Koneksi";
     //}
